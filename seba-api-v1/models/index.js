6d927499e9b473aa47f40873ee7641139d4b0158
livelikeabel <esung1129@gmail.com>
2018-12-15 17:47:41 +0900

    [#3] Add User model

--- seba-api-v1/models/index.js
+++ seba-api-v1/models/index.js
@@ -9,4 +9,6 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.User = require('./user')(sequelize, Sequelize);
+
 module.exports = db;
