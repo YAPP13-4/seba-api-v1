baab1f12be422ec252e584eb9fb7e511d077f65b
ERTuringLee <ehdspt@naver.com>
2019-01-05 17:23:45 +0900

    [#26]fix: 객체로 넘겨주도록 설정을 바꿈

--- seba-api-v1/routes/featured.js
+++ seba-api-v1/routes/featured.js
@@ -9,11 +9,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  const {type, time, music_id} = req.body;
+  const {type, time, music} = req.body;
   models.Featured.create({
     type,
     time,
-    music_id
+    music
   }).then((featured) => res.status(201).json(featured));
 });
 
