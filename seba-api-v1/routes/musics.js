02503fd0445f9727445f44da1535595fa6d6fb08
JungSeBin <tpqls5144@naver.com>
2019-02-02 16:28:50 +0900

    [#35] 음악 등록시 url에 맞는 기본데이터 제공

--- seba-api-v1/routes/musics.js
+++ seba-api-v1/routes/musics.js
@@ -25,6 +25,24 @@ router.get('/seba-choice', function(req, res, next) {
     });
 });
 
+router.get('/register-form', function(req, res) {
+  const url = req.query.url;
+  request(`https://api.soundcloud.com/resolve.json?url=${url}&client_id=${clientId}`, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const { title, user, description, artwork_url } = JSON.parse(body);
+      console.log(title);
+      res.status(200).json({
+        title: title,
+        musician: user.username,
+        description: description,
+        artworkImg: artwork_url
+      })
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
 router.post('/', function(req, res, next) {
   let url = req.body.url;
   request(`https://api.soundcloud.com/resolve.json?url=${url}&client_id=${clientId}`, function(error, response, body) {
