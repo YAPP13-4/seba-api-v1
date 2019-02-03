bacd91e65a21d22b34ba12788f2da8b8fc820eb3
JungSeBin <tpqls5144@naver.com>
2019-02-03 23:58:31 +0900

    [#37] swagger music api 에만 적용

--- seba-api-v1/routes/musics.js
+++ seba-api-v1/routes/musics.js
@@ -7,7 +7,22 @@ var router = express.Router();
 
 const clientId = 'a281614d7f34dc30b665dfcaa3ed7505';
 
-router.get('/seba-choice', function(req, res, next) {
+/**
+ * @swagger
+ * /musics/seba-choice:
+ *   get:
+ *     summary: seba's 추천 음악 리스트 가져오기
+ *     tags: [Music]
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         schema:
+ *           type: "array"
+ *           items:
+ *             $ref: "#/definitions/Music"
+ */
+router.get('/seba-choice', function (req, res, next) {
   models.Music
     .findAll({
       where: {
@@ -25,9 +40,28 @@ router.get('/seba-choice', function(req, res, next) {
     });
 });
 
-router.post('/', function(req, res, next) {
+/**
+ * @swagger
+ * /musics:
+ *   post:
+ *     summary: 음악 등록
+ *     tags: [Music]
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         schema:
+ *           $ref: "#/definitions/Music"
+ *       400:
+ *         description: 이미 존재하는 음악 url 입력
+ *       403:
+ *         description: 미 로그인 유저의 요청
+ *       404:
+ *         description: 음악정보가 없는 url 입력
+ */
+router.post('/', function (req, res, next) {
   let url = req.body.url;
-  request(`https://api.soundcloud.com/resolve.json?url=${url}&client_id=${clientId}`, function(error, response, body) {
+  request(`https://api.soundcloud.com/resolve.json?url=${url}&client_id=${clientId}`, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       const { title, user, description, artwork_url, duration, stream_url, created_at } = JSON.parse(body);
 
@@ -62,7 +96,22 @@ router.post('/', function(req, res, next) {
   });
 });
 
-router.get('/:id', function(req, res, next) {
+/**
+ * @swagger
+ * /musics/:id:
+ *   get:
+ *     summary: 음악 상세 data
+ *     tags: [Music]
+ *     parameters:
+ *     responses:
+ *       200:
+ *         description: 성공
+ *         schema:
+ *           $ref: "#/definitions/MusicDetail"
+ *       404:
+ *         description: 해당 음악이 없음
+ */
+router.get('/:id', function (req, res, next) {
   const id = req.params.id;
 
   models.Music
@@ -79,7 +128,11 @@ router.get('/:id', function(req, res, next) {
       ]
     })
     .then(music => {
-      res.json(music);
+      if (music) {
+        res.json(music);
+      } else {
+        res.status(404).json({ error: "해당 음악이 없습니다." });
+      }
     });
 });
 
