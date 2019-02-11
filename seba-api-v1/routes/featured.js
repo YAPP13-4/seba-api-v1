f12555b0b2c2fb72be4e8a11c5fc148c50db93eb
SkynI25 <rlxo1651@daum.net>
2019-02-11 22:29:30 +0900

    [#30] featured type에 따른 순위리스트

--- seba-api-v1/routes/featured.js
+++ seba-api-v1/routes/featured.js
@@ -1,27 +1,54 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
-const models = require('../models');
+const models = require("../models")
 
-router.get('/', function(req, res, next) {
-  models.Featured.findAll()
-  .then(featured => res.json(featured));
-});
+Object.filter = (obj, predicate) =>
+  Object.keys(obj)
+  .filter(key => predicate(obj[key]))
+  .reduce((res, key) => (res[key] = obj[key], res), {});
 
-router.post('/', function(req, res, next) {
-  const {type, music} = req.body;
-  let time = req.body.time;
-  time = time /1000;
+router.get("/", function (req, res, next) {
+  models.Featured.findAll().then(featured => res.json(featured))
+})
+
+router.post("/", function (req, res, next) {
+  const {
+    type,
+    music
+  } = req.body
+  let time = req.body.time
+  time = time / 1000
   models.Featured.create({
     type,
     time,
     music
-  }).then((featured) => res.status(201).json(featured));
-});
+  }).then(featured => res.status(201).json(featured))
+})
 
-router.get('/:id', function(req,res,next) {
-  const music_id = req.params.id;
-  models.Featured.findAll({where: {music_id}})
-  .then(featured => res.json(featured));
-});
-module.exports = router;
+router.get("/:id", function (req, res, next) {
+  const music_id = req.params.id
+  models.Featured.findAll({
+    where: {
+      music_id
+    }
+  }).then(featured => res.json(featured))
+})
+
+router.get("/type/:data", function (req, res, next) {
+  const music_type = req.params.data
+  models.Featured.findAll({
+      where: {
+        type: music_type
+      },
+      include: {
+        model: models.Music,
+        required: true
+      },
+      raw: true
+    })
+    .then(featured => {
+      res.json(featured)
+    });
+})
+module.exports = router
